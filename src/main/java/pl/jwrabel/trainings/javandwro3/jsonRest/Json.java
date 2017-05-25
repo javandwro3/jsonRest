d956928278d379778352b72071c520e2206232ad
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-05-25 20:30:12 +0200

    update

--- src/main/java/pl/jwrabel/trainings/javandwro3/jsonRest/Json.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/jsonRest/Json.java
@@ -5,18 +5,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by jakubwrabel on 25/05/2017.
  */
 public class Json {
-	public static void main(String[] args) throws JsonProcessingException {
+	public static void main(String[] args) throws IOException {
 		{
 			ObjectMapper objectMapper = new ObjectMapper();
 			objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
 
+			// ZAMIANA OBIEKTU NA JSON (String)
 			Point point = new Point();
 			point.setX(100);
 			point.setY(300);
@@ -26,62 +30,31 @@ public class Json {
 			System.out.println(pointJson);
 
 
+			// ZAPIS JSONA DO PLIKU
+			objectMapper.writeValue(new File("points.json"), point);
+
+			List<Point> pointList = new ArrayList<>();
+			pointList.add(new Point());
+			pointList.add(new Point());
+			pointList.add(new Point());
+			objectMapper.writeValue(new File("pointsList.json"), pointList);
 
 
+			// ZAMIANA JSONa (Stringa, napisu) na obiekt Javovy
+			String pointsJsonString = "{\n" +
+					"  \"x\" : 100,\n" +
+					"  \"y\" : 300\n" +
+					"}";
 
+			Point point1 = objectMapper.readValue(pointsJsonString, Point.class);
 
+			// ZAMIANA JSONA Z PLIKU NA OBIEKT
+			Point point2 = objectMapper.readValue(new File("points.json"), Point.class);
+			List<Point> pointsListFromFile = objectMapper.readValue(new File("pointsList.json"), List.class);
 
+			// UWAGA
+			// 1.
 
-
-
-
-
-
-//			ObjectMapper objectMapper = new ObjectMapper();
-//			// włączenie formatowania wyjścia
-//
-//			// ZAMIANA OBIEKTU NA JSON
-//			Place place = new Place("Dom", "Warszawa", Arrays.asList(new Point(100, 200)));
-//			String placeJson = objectMapper.writeValueAsString(place);
-//			System.out.println(placeJson);
-//
-//			// ZAPIS JSONA DO PLIKU
-//			objectMapper.writeValue(new File("placeJackson.json"), place);
-//
-//			String placeAsJson = "{\n" +
-//					"  \"name\" : \"Dom\",\n" +
-//					"  \"city\" : \"Warszawa\",\n" +
-//					"  \"points\" : [ {\n" +
-//					"    \"x\" : 100,\n" +
-//					"    \"y\" : 200\n" +
-//					"  } ]\n" +
-//					"}";
-//
-//			// ZAMIANA JSONA NA OBIEKT
-//			Place placeReader = objectMapper.readValue(placeAsJson, Place.class);
-//
-//			// ZAMIANA JSONA Z PLIKU NA OBIEKT
-////		Place placeReaded = objectMapper.readValue(new File("cust.json"), Place.class);
-//
-//			// 1. Stworzyć klasę odwzorowującą poniższego JSONA
-////		Customer/Person
-//			String jsonString = "{\"firstName\":\"Adam\",\"lastName\":\"Kowalski\",\"birthYear\":1980,\"idNumber\":\"ABC\"}";
-//
-//
-//			String jsonString2 = "{\n" +
-//					"\"coord\": {\n" +
-//					"\"lon\": 139,\n" +
-//					"\"lat\": 35\n" +
-//					"},\n" +
-//					"\"weather\": [\n" +
-//					"{\n" +
-//					"\"id\": 800,\n" +
-//					"\"main\": \"Clear\",\n" +
-//					"\"description\": \"clear sky\",\n" +
-//					"\"icon\": \"01n\"\n" +
-//					"}\n" +
-//					"]}";
-//			System.out.println(jsonString2);
 		}
 	}
 }
